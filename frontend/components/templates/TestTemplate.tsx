04a28513ae7627ee5e4e4a46eff4c0e502399a23
verd <37105170+ellsurge@users.noreply.github.com>
2025-03-31 07:56:35 +0000

    Enhance TestTemplate layout and update Tamagui configuration for responsive design

--- frontend/components/templates/TestTemplate.tsx
+++ frontend/components/templates/TestTemplate.tsx
@@ -1,4 +1,4 @@
-import { YStack, Text, XStack } from "tamagui";
+import { YStack, Text, XStack, Stack, View } from "tamagui";
 import EventList from "../organisms/EventList";
 import React from "react";
 import AddEvent from "../organisms/AddEvent";
@@ -6,28 +6,32 @@ import AddEvent from "../organisms/AddEvent";
 // Reuse the prop type from EventList
 type EventListProps = React.ComponentProps<typeof EventList>;
 
-type props ={
+type props = {
     eventList: EventListProps;
     addEvent: React.ComponentProps<typeof AddEvent>; // Function to handle add action
-}
-export default function TestTemplate( props: props) {
+};
+
+export default function TestTemplate(props: props) {
     return (
         <YStack
             p={16}
-            flex={1}    
+            flex={1}
             verticalAlign="center"
             bg="#f5f5f5"
             gap={10}
         >
-                        <Text fontSize={24} fontWeight="bold" color="#333">
+            <Text fontSize={24} fontWeight="bold" color="#333">
                 Test Screen
-            </Text> 
-            <XStack width={"100%"} gap={10} justify="center">
-                
-            <AddEvent {...props.addEvent} />
-            <EventList {...props.eventList} />
-         </XStack>
-
+            </Text>
+            <YStack
+                $md={{ flexDirection: "row" }}
+                width={"100%"}
+                gap={10}
+                justify="center"
+            >
+                <AddEvent {...props.addEvent} />
+                <EventList {...props.eventList} />
+            </YStack>
         </YStack>
     );
 }
